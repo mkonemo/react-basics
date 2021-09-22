727fe4ddba2099f929a482a56c8fb6d5b490cc99
mkonemo <mkonemo@gmail.com>
2021-09-22 16:47:55 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -15,29 +15,7 @@ import Datalist from './components/datalist/DataList';
 
 import './style.css';
 
-const App = () => {
-
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14)
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28)
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12)
-    }
-  ];
+const App = () => {  
 
   const inputTextHandler = inputText => {
     console.log(inputText);
@@ -118,7 +96,7 @@ but in a parent component.</p>
           <Json />
         </li>
         <li>
-          <Datalist items={expenses}/>
+          <Datalist />
         </li>
       </ol>
     </div>
--- src/components/datalist/DataList.js
+++ src/components/datalist/DataList.js
@@ -4,6 +4,41 @@ import Card from '../UI/Card';
 
 const DataList = (props) => {
   
+  const sampleData = [
+    {
+      firstName: 'Mko',
+      lastName: 'Nemo',
+      age: 48
+    },
+    {
+      firstName: 'MIrko',
+      lastName: 'Niciarelli',
+      age: 48,
+      info:
+        'Lorem Ipsum'
+    }
+  ];
+
+  
+
+  const sampleExpenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14).toDateString()
+    },
+    {
+      id: 'e2',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12).toDateString()
+    }
+  ];
+
+  const sampleJson = fetch('https://fakestoreapi.com/products/1')
+  .then(res=>res.json())
+  .then(json=>console.log(json));
 
   return (
     <div>
@@ -11,15 +46,26 @@ const DataList = (props) => {
       <Card>
         <ul>
           
-        {props.items.map(expense => (
+        {sampleData.map(item => (
           <li>
-            {expense.title}
-            {expense.amount}
-            {expense.date}
+            {item.firstName} {item.lastName}, ({item.age} anni).<br/>{item.info}
           </li>
         ))}
         </ul>
       </Card>
+      <Card>
+        <ul>
+        {sampleExpenses.map(expense => (
+          <li>
+            {expense.title} {expense.amount}&euro;, ({expense.date})
+          </li>
+        ))}
+        
+        </ul>
+      </Card>
+      <li>{sampleJson.title}</li>
+      <Card>
+        </Card>
     </div>
   );
 };
