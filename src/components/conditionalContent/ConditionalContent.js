d8c41bc30b7d081d58debcfce4322d1723755856
mkonemo <mkonemo@gmail.com>
2021-09-23 16:40:37 +0200

    1 file modified

--- src/components/conditionalContent/ConditionalContent.js
+++ src/components/conditionalContent/ConditionalContent.js
@@ -6,7 +6,13 @@ const ConditionalContent = () => {
   return (
     <div>
       <h3>Conditional content (3 different examples)</h3>
-      <Card></Card>
+      <Card>
+        <pre><code>let conditionalContent = &lt;p&gt;Default content&lt;/p&gt;
+          if (condition = true) &lbrace;
+            conditionalContent = &lt;p&gt;Content if condition is true&lt;/p&gt;
+          &rbrace;
+        </code></pre>
+      </Card>
     </div>
   );
 };
