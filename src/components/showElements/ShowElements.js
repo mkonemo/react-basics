b8f232d0a38832f75dcc82d0e8b5128bc400b839
mkonemo <mkonemo@gmail.com>
2021-09-24 17:12:23 +0200

    1 file modified

--- src/components/showElements/ShowElements.js
+++ src/components/showElements/ShowElements.js
@@ -5,10 +5,19 @@ import Card from '../UI/Card';
 const ShowElements = () => {
   const [isShown, setIsShown] = useState(false);
 
+  const ShowHandler = () => {
+    console.log('1' + isShown);
+
+    isShown = true ? (isShown = false) : (isShown = true);
+
+    console.log('2' + isShown);
+  };
+
   return (
     <div>
       <h3>Show elements</h3>
-      <button>Show element</button>
+      {!isShown && <button onClick={ShowHandler}>Show element</button>}
+      {isShown && <button onClick={ShowHandler}>Hide element</button>}
       <br />
       <br />
       <Card>
