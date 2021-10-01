07ded88c6049011c4a5d662a09ad516bfe20acc8
mkonemo <mkonemo@gmail.com>
2021-10-01 14:43:41 +0200

    1 file added

---
+++ src/components/styles/DynamicInlineStyles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Card from './UI/Card';
+
+const DynamicInlineStyles = () => {
+  return (
+    <div>
+      <Card>Esempio</Card>
+    </div>
+  );
+};
+
+export default DynamicInlineStyles;
