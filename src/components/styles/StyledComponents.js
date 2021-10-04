4b45d2f1a184925791096e135271c180cff7702c
mkonemo <mkonemo@gmail.com>
2021-10-04 17:00:05 +0200

    1 file modified, 1 added

--- src/components/styles/StyledComponents.js
+++ src/components/styles/StyledComponents.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const StyledComponents = () => {
-  return (
-    <div>StyledComponents</div>
-  );
-}
+import './StyledComponents.css';
 
-export default StyledComponents; 
+const StyledComponents = () => {
+  const [isValid, setIsValid] = useState(true);
+
+  const stateChangeHandler = () => {
+    setIsValid(!isValid);
+  };
+  let variable = isValid.toString();
+  return (
+    <div>
+      <p>
+        The style of the paragraph is set depending on the value of the 
+        <samp>isValid</samp> value.
+      </p>
+      <Card>
+        <samp style={{ color: isValid ? 'olive' : 'orange' }}>
+          isValid = '{variable}'
+        </samp>
+        <br />
+        <br />
+        <button onClick={stateChangeHandler}>Change isValid</button>
+      </Card>
+    </div>
+  );
+};
+
+export default StyledComponents;
