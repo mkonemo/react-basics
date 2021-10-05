97f807cdabb4675a288d9b7e052d36dbdcf8030e
mkonemo <mkonemo@gmail.com>
2021-10-05 17:42:49 +0200

    1 file modified

--- src/components/styles/StyledComponents.js
+++ src/components/styles/StyledComponents.js
@@ -19,14 +19,24 @@ const StyledComponents = () => {
         styles into the app there are two different approaches.
       </p>
       <h4>Styled components</h4>
-      <p>
+      
+      <p>Styled components are imported from a library, which can be found at the following link:<br/>
         <a href="www.styled-components.com">www.styled-components.com</a>
       </p>
       <Card>
+        
         <p>The following code shows a peculiar syntax, called <em>"Attacked template literal"</em></p>
         <samp>
           import styled from 'styled-components' 
-          const Button = styled.button``;
+          const Button = styled.button`
+          font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+          `;
         </samp>
       </Card>
     </div>
