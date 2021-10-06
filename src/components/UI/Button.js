6517af06c2f7334313bf63d16ce97907192e733e
mkonemo <mkonemo@gmail.com>
2021-10-06 17:30:53 +0200

    1 file modified

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #666;
@@ -10,6 +10,10 @@ const Button = styled.button`
   background: ${props => props.primary ? "red" : "#666"};;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
 &:focus {
   outline: none;
