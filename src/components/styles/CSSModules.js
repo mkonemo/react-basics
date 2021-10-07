f21cf0c184df0127be8a2ef301d262b1cf47da91
mkonemo <mkonemo@gmail.com>
2021-10-07 15:56:01 +0200

    1 file modified

--- src/components/styles/CSSModules.js
+++ src/components/styles/CSSModules.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const CSSModules = () => {
-  return(
-    <div>CSS modules are an alternative ti Styled Components to manage scoped styles.</div>
-  )
-}
+  return (
+    <div>
+      <p>
+        CSS modules are an alternative to Styled Components to manage scoped
+        styles.
+      </p>
+      <p>CSS modules automatically generate unique custom classes, so each CSS is specific to the component where it is defined.</p>
+      <p>In order to implement them you need to name the the CSS file like <samp>style.module.css</samp> and import it with a specific syntax.</p>
+    </div>
+  );
+};
 
 export default CSSModules;
