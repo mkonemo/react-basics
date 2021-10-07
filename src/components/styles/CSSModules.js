cf28da512be0ce5ebbfcf5636293c2be12473abc
mkonemo <mkonemo@gmail.com>
2021-10-07 17:19:12 +0200

    1 file modified

--- src/components/styles/CSSModules.js
+++ src/components/styles/CSSModules.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Card from '../UI/Card';
+
+
 const CSSModules = () => {
   return (
     <div>
@@ -9,6 +12,11 @@ const CSSModules = () => {
       </p>
       <p>CSS modules automatically generate unique custom classes, so each CSS is specific to the component where it is defined.</p>
       <p>In order to implement them you need to name the the CSS file like <samp>style.module.css</samp> and import it with a specific syntax.</p>
+      <Card>
+        <p>This is the syntax to import a CSS module:</p>
+        <samp>import styles from './Button.module.css';</samp>
+        
+      </Card>
     </div>
   );
 };
