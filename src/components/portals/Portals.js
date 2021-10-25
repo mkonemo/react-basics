311021359de153af8edc89c91c261f9c115e9218
mkonemo <mkonemo@gmail.com>
2021-10-25 17:40:37 +0200

    1 file modified

--- src/components/portals/Portals.js
+++ src/components/portals/Portals.js
@@ -5,9 +5,17 @@ const Portals = () => {
     <React.Fragment>
       <h4>Fragments</h4>
       <h4>Portals</h4>
-      <p>Portals allow to put some element into a specific component, but then render it in a different position in the final HTML file (es. overlays, menu, etc.)</p>
+      <p>
+        Portals allow to put some element into a specific component, but then
+        render it in a different position in the final HTML file (es. overlays,
+        menu, etc.)
+      </p>
       <h4>Refs</h4>
       <p>Refs allow to get access to other DOM elements</p>
+      <p>
+        With refs, we can set up a connection between a HTML element which is
+        being rendered in the end and our other JavaScript code.
+      </p>
     </React.Fragment>
   );
 };
